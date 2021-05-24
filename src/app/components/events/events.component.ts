9b9f446e0d754345078b91ca8dad9a41260b2d22
naguire2018104 <naguirre-2018104@kinal.edu.gt>
2021-05-23 19:47:59 -0600

    Events

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,4 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Event } from 'src/app/models/event'
+import { Hotel } from 'src/app/models/hotel';
+import { Room } from 'src/app/models/room';
+import { RestEventService } from 'src/app/services/restEvent/rest-event.service';
+import { RestRoomService } from 'src/app/services/restRoom/rest-room.service';
 
 @Component({
   selector: 'app-events',
@@ -7,9 +13,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  hotel: Hotel;
+  room: Room;
+  event: Event;
+  events: Array<Event> = [];
+  roomsEvent: Array<Room> = [];
+  typesEvent = ["Private","Public"];
+  confirmPassword;
+
+  constructor(private restEvent: RestEventService, private datepipe: DatePipe) { 
+    this.room = new Room("","",null,null,null,"");
+    this.event = new Event("","","",null);
+    this.hotel = new Hotel("", "", "", "", null, "", "", [], []);
+  }
 
   ngOnInit(): void {
+    this.roomsEvent = [];
+    this.events = [];
+
+    this.hotel = JSON.parse(localStorage.getItem("hotel"));
+
+    this.restEvent.getEventsByHotelAdmin().subscribe((resp:any)=>{
+      resp.events.forEach(element => {
+        this.events.push(element);
+      });
+    })
+
+    this.restEvent.getRoomsEvent().subscribe((resp:any)=>{
+      resp.rooms.forEach(element => {
+        this.roomsEvent.push(element);
+      });
+    })
+  }
+
+  onSubmit(saveEventByAdminForm){
+    this.restEvent.createEvent(this.event).subscribe( (resp:any) => {
+      if(resp.eventSaved){
+        alert(resp.message);
+        saveEventByAdminForm.reset();
+        this.event = resp.eventSaved;
+      }else {
+        alert(resp.message);
+      }
+    }, 
+    err => alert(err.error.message));
+  }
+
+  updateEvent(updateEventByAdminForm){
+    var eventId = this.event._id;
+    this.restEvent.updateEvent(eventId,this.event).subscribe( (resp:any) => {
+      if(resp.eventUpdated){
+        alert(resp.message);
+        updateEventByAdminForm.reset();
+        this.event = resp.eventUpdated;
+      }else {
+        alert(resp.message);
+      }
+    }, 
+    err => alert(err.error.message));
+  }
+
+  deleteInfo(){
+    this.event = new Event("","","",null);
+  }
+
+  setEvent(event: Event){
+    this.event = event;
+  }
+
+  showDateInFormat(date): Date{
+    date = this.datepipe.transform(date, "dd/MM/yyyy");
+    return date;
+  }
+
+  setRoom(room: Room){
+    this.room = room;
+  }
+
+  removeEvent(){
+    this.restEvent.deleteEvent(this.event._id,this.confirmPassword).subscribe((resp:any)=>{
+      if(resp.ok == true){
+        alert(resp.message);
+        this.ngOnInit();
+      }else{
+        alert(resp.message);
+      }
+    },
+    error => alert(error.error.message))
   }
 
 }
